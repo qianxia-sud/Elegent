e04bbb47de021325eeb8391d28bce275581fdac2
unknown <jianxiaremo@qq.com>
2025-12-02 10:55:57 +0800

    初始化项目结构：添加核心代码与配置文件

---
+++ src/main/java/com/ecommerce/ecommerceauth/EcommerceAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerceauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/ecommerceauth/EcommerceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerceauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
